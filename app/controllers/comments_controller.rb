0f51effdfaa37d20eb24b6b4c08db163a13581e8
mr-shimizu <toshihiro_s_89@yahoo.co.jp>
2018-04-28 12:22:24 +0900

    Add params require.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.permit(:content, :prototype_id, :user_id)
+    params.require(:comment).permit(:content, :prototype_id, :user_id)
   end
 end
