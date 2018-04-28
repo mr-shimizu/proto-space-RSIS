e737d960671f54848fc4df5bfdce3d441b46dc54
mr-shimizu <toshihiro_s_89@yahoo.co.jp>
2018-04-28 12:52:49 +0900

    Add "Destroy" function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,15 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    comment = Comment.find(params[:id])
+    @prototype = Prototype.find(params[:prototype_id])
+    if comment.user_id == current_user.id
+      comment.destroy
+    end
+    redirect_to prototype_path(@prototype.id)
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:content, :prototype_id, :user_id)
