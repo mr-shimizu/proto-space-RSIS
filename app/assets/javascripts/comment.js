d7f13692ba059cc6760dc9823d8e763054331d07
mr-shimizu <toshihiro_s_89@yahoo.co.jp>
2018-04-30 16:50:52 +0900

    Send the Ajax to the controller.
    Set click event to the submit button and send form data by Ajax to
    “comments” controller, “create” action.

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,2 +1,15 @@
 $(document).on("turbolinks:load",function(){
+  $('#new_comment').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+      $.ajax({
+        url: url,
+        type: 'POST',
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+  });
 });
