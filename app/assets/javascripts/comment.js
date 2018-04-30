ffdbff8b8b6618b4950505715462a2d12a8a916a
mr-shimizu <toshihiro_s_89@yahoo.co.jp>
2018-04-30 16:54:00 +0900

    Define the new comment HTML

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,4 +1,23 @@
 $(document).on("turbolinks:load",function(){
+  function buildHTML(comment){
+        var html = `<div class="media">
+                      <div class="media-left">
+                        <a href="/users/${comment.user_id}"><img alt="profile_photo" class="media-object" style="width: 64px; height: 64px;" src=${comment.avatar_url}>
+                        </a></div>
+                        <div class="media-body">
+                          <h4 class="media-heading" id="top-aligned-media">
+                            ${comment.user.name}
+                            <a rel="nofollow" data-method="delete" href="/prototypes/${comment.prototype_id}/comments/${comment.id}">削除</a>
+                            <a href="/prototypes/${comment.prototype_id}/comments/${comment.id}/edit">編集</a>
+                          </h4>
+                          <p>
+                            ${comment.content}
+                          </p>
+                        </div>
+                      </div>`
+
+  return html;
+  }
   $('#new_comment').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
