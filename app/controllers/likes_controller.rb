b7aa9d8c496de24b41a2f3f31221a822fa12d2f1
bbz8 <tech-camp@1-084.local>
2018-05-01 20:18:13 +0900

    argument_correct
    ビュー（LIKEボタン）に渡す引数を修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,19 @@
 class LikesController < ApplicationController
   def create
-    @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
-    @likes = Like.where(prototype_id: params[:prototype_id])
-    @prototypes = Prototype.all
+    @like = Like.create(like_params)
+    redirect_to prototype_path(id: params[:prototype_id])
   end
 
   def destroy
-    @like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
-    like.destroy
-    @likes = Like.where(prototype_id: params[:prototype_id])
-    @tweets = Prototype.all
+    @like = Like.find(params[:id])
+    @like.destroy
+    redirect_to prototype_path(id: params[:prototype_id])
   end
+
+private
+
+  def like_params
+    params.permit(:prototype_id).merge(user_id: current_user.id)
+  end
+
 end
