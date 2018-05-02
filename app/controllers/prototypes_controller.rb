b208739274ebcabd32b9ee6e38dcb9f580b71de6
kei-ozaki <nya33n@gmail.com>
2018-05-02 18:04:57 +0900

    add strong parameter

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -49,6 +49,7 @@ class PrototypesController < ApplicationController
       :catch_copy,
       :concept,
       :user_id,
+      :tag_list,
       captured_images_attributes: [:content, :status]
     )
   end
