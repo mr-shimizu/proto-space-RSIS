3d9fe4e889adf2311a005108791f0e920398f4ca
mr-shimizu <toshihiro_s_89@yahoo.co.jp>
2018-05-07 19:45:00 +0900

    Add two tests for User model validation.
    one is without name.
    another one is without email.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,1 +1,16 @@
 require 'rails_helper'
+
+describe User do
+  describe '#create' do
+    it "is invalid without a name" do
+      user = build(:user, name: "")
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")
+    end
+    it "is invalid without an email" do
+      user = build(:user, email: "")
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+  end
+end
