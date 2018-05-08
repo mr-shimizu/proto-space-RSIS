702103228b51fafaa0479b4a48bfa6411cab495a
kei-ozaki <nya33n@gmail.com>
2018-05-08 13:46:19 +0900

    likes controllerを編集
    変数をまとめて渡す
    リダイレクトを外す

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,21 @@
 class LikesController < ApplicationController
+  before_action :set_variables, only: [:create, :destroy]
+
   def create
     @like = Like.create(like_params)
-    redirect_to prototype_path(params[:prototype_id])
   end
 
   def destroy
-    @like = Like.find(params[:id])
-    @like.destroy
-    redirect_to prototype_path(params[:prototype_id])
+    like = Like.find(params[:id])
+    like.destroy
   end
 
 private
+
+  def set_variables
+    @prototype = Prototype.find(params[:prototype_id])
+    @likes = Like.where(params[:prototype_id])
+  end
 
   def like_params
     params.permit(:prototype_id).merge(user_id: current_user.id)
