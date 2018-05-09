636caf5484fcc2e7849dd65136087d0b84f25da8
kei-ozaki <nya33n@gmail.com>
2018-05-09 16:31:10 +0900

    add routing search

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
   resources :users, only: [:show, :edit, :update]
-  resources :tags, only: [:index, :show], param: :tag_name
+  resources :tags, only: [:index, :show], param: :tag_name do
+    collection do
+      get 'search'
+    end
+  end
   resources :prototypes do
     resources :likes, only: [:index, :create, :destroy]
     resources :comments, only: [:create, :destroy, :edit, :update]
