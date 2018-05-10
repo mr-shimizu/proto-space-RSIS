87839880818db728703bd0c3d3a8783cc5465cad
kei-ozaki <nya33n@gmail.com>
2018-05-10 19:07:29 +0900

    create factorygirl

--- spec/factories/prototypes.rb
+++ spec/factories/prototypes.rb
@@ -4,10 +4,47 @@ FactoryGirl.define do
     title        {Faker::Lorem.word}
     catch_copy   {Faker::Lorem.sentence}
     concept      {Faker::Lorem.sentence}
-    user
-    tag_list     nil#{Faker::Lorem.words(3)}
-    # captured_images_attributes [:content, :status, :id]
     created_at   {Faker::Time.between(2.days.ago, Time.now, :all)}
-    likes_count  nil
+    user
+
+    trait :with_sub_images do
+      transient do
+        sub_images_count 3
+      end
+
+      after(:create) do |prototype, evaluator|
+        prototype.captured_images << create_list(:sub_image, evaluator.sub_images_count)
+      end
+    end
+
+    trait :with_tags do
+      transient do
+        tags_count 3
+      end
+
+      after(:create) do |prototype, evaluator|
+        prototype.tags << create_list(:tag, evaluator.tags_count)
+      end
+    end
+
+    trait :with_comments do
+      transient do
+        comments_count 5
+      end
+
+      after(:create) do |prototype, evaluator|
+        prototype.comments << create_list(:comment, evaluator.comments_count)
+      end
+    end
+
+    trait :with_likes do
+      transient do
+        likes_count 5
+      end
+
+      after(:create) do |prototype, evaluator|
+        prototype.likes << create_list(:like, evaluator.likes_count)
+      end
+    end
   end
 end
