97ffb9c1f13fa5692a4aff418cc344b7cfe781e1
kei-ozaki <nya33n@gmail.com>
2018-05-10 20:47:04 +0900

    test validations exist

--- spec/models/prototype_spec.rb
+++ spec/models/prototype_spec.rb
@@ -5,7 +5,16 @@ describe Prototype do
     it 'is assocciated with a user' do
       user = create(:user)
       prototype = create(:prototype, user: user)
-      expect(prototype.user).to eq user
+      expect(prototype.user) == user
+    end
+  end
+
+  describe 'validations' do
+    context 'validations exist' do
+      it 'has the validations' do
+        prototype = build(:prototype)
+        expect(prototype).to be_valid
+      end
     end
   end
 end
