89b0cdd9e67bd775395c0a53268d21128e91bb55
mr-shimizu <toshihiro_s_89@yahoo.co.jp>
2018-05-11 17:16:46 +0900

    made a factory of captured_image model

--- spec/factories/prototypes.rb
+++ spec/factories/prototypes.rb
@@ -5,8 +5,6 @@ FactoryGirl.define do
     catch_copy   {Faker::Lorem.sentence}
     concept      {Faker::Lorem.sentence}
     user
-    tag_list     nil#{Faker::Lorem.words(3)}
-    # captured_images_attributes [:content, :status, :id]
     created_at   {Faker::Time.between(2.days.ago, Time.now, :all)}
     likes_count  nil
   end
