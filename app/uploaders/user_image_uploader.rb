5b8f840911ee956442df03751feaf57256cda8b3
mr-shimizu <toshihiro_s_89@yahoo.co.jp>
2018-05-17 11:22:08 +0900

    storage

--- app/uploaders/user_image_uploader.rb
+++ app/uploaders/user_image_uploader.rb
@@ -7,7 +7,7 @@ class UserImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -49,9 +49,6 @@ class UserImageUploader < CarrierWave::Uploader::Base
   # end
 
   def default_url
-      '/uploads/noimage.png'
+    '/uploads/noimage.png'
   end
-
-
-
 end
